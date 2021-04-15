17284587b02bf7f7e1bc736bd1d71bfc7ad5f442
Connor Walden <connormwalden@gmail.com>
2021-04-15 19:59:03 +0930

    Updated workout model to better fit

--- models/workout.js
+++ models/workout.js
@@ -5,40 +5,33 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
     type: {
         type: String,
-        required: "Enter the type of the exercise"
     },
     name: {
         type: String,
-        required: "Enter the name of the exercise"
     },
     duration: { 
         type: Number,
-        required: "Enter the duration of the exercise"
     },
     weight: {
         type: Number,
-        required: "Enter the weight of the exercise"
     },
     reps: {
         type: String,
-        required: "Enter the reps for the exercise"
     },
     sets: { 
         type: String,
-        required: "Enter the sets for the exercise"
     }
 });
 
 const workoutSchema = new Schema({
   day: {
-    type: Number,
-    required: "Enter a day for the workout"
+    type: Date,
   },
-  excercises: [
+  exercises: [
       exerciseSchema
   ],
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("workouts", workoutSchema);
 
 module.exports = Workout;
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,41 @@
 const router = require('express').Router();
+const Workout = require('../models/workout');
 
 router.get('/api', (req, res) => {
     res.status(200).json({ message: 'Success!' });
+});
+
+router.get('/api/workouts', (req, res) => {
+    Workout.find({}).sort({ day: -1 })
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json( err );
+    });
+});
+
+router.post('/api/workouts', (req, res) => {
+    Workout.create(req.body)
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json( err );
+    });
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.updateOne({ id: req.params.id }, req.body)
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json( err );
+    });
 });
 
 module.exports = router;
